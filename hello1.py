bdf8dc80183886a0165e79571f95d50f96099d83
iht_wang <iht_wang@126.com>
2018-03-23 21:45:49 +0800

    'teststst'

--- hello1.py
+++ hello1.py
@@ -1,2 +1,14 @@
 #coding=utf-8
-print 'hello world!'
+from flask import Flask
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
